d6d85509b83a1d3e80bcd588f403abc96d25f056
N.R.T <nathsharwor@hotmail.com>
2017-04-02 18:32:01 +0800

    TEMP

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<head>
+	<head>
+		<title>Pulma</title>
+		<script src="main.js"></script>
+	</head>
+	<body>
+		<div id="header"></div>
+		<div id="content"></div>
+		<div id="footer"></div>
+	</body>
+</head>
---
+++ main.js
@@ -0,0 +1,6 @@
+//Version id - using semantic versioning
+var version = "0.0.1";
+
+window.onload = function () {
+	//ONLOAD EVENT
+};
