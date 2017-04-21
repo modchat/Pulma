83e53d4bbd2e1035523f15a80858cfbd84b92acc
N.R.T <nathsharwor@hotmail.com>
2017-04-21 14:27:59 +0800

    Fixes, and class framework
    - Class framework is not going to be used - and will promptly be removed - however it will still be stored in this commit, if for some reason it might need to be used
    - FIxed the annoying CSS span offshoot bug

--- index.php
+++ index.php
@@ -1,23 +1,77 @@
 <?php
-//Write data
-$width = 14;
-$height = 6;
-$xml = new DOMDocument();
-$xml->formatOutput = true;
-$board = $xml->appendChild($xml->createElement("board"));
-$board->setAttribute("width", $width);
-$board->setAttribute("height", $height);
-for ($h = 0; $h < $height; $h++)
-	for ($w = 0; $w < $width; $w++)
-		$tile = $board->appendChild($xml->createElement("tile", !!!random_int(0, 14)?"CAT":".."));
-$xml->save("txml.xml");
+//Create a new xml board
+function newXML() {
+	$width = 14;
+	$height = 6;
+	$xml = new DOMDocument();
+	$xml->formatOutput = true;
+	$board = $xml->appendChild($xml->createElement("board"));
+	$board->setAttribute("width", $width);
+	$board->setAttribute("height", $height);
+	for ($h = 0; $h < $height; $h++) {
+		for ($w = 0; $w < $width; $w++) {
+			$tile = $board->appendChild($xml->createElement("tile"));
+			if (!!!random_int(0, 14)) {
+				$unit = $tile->appendChild($xml->createElement("unit"));
+				$unit->setAttribute("type", "cat");
+			}
+		}
+	}
+	$xml->save("txml.xml");
+}
 
-//Read data
-$dom = simplexml_load_file('txml.xml');
-//xpath('/board/tile')
-//xpath('//tile[contains(unit, "Cat")]/@hp')
-$height = $dom->xpath('/board/@height')[0];
-$width = $dom->xpath('/board/@width')[0];
+//Read xml board
+function readXML() {
+	$dom = new DOMDocument();
+	if ($dom->load("txml.xml")) {
+		return $dom;
+	}
+	return false;
+}
+
+$dom = readXML();
+$board = $dom->getElementsByTagName("board")->item(0);
+$width = $board->getAttribute("width");
+$height = $board->getAttribute("height");
+
+class unit {
+	public $name = 'Tom';
+	
+	private $type = 'blank';
+	public $short = '..';
+	private $health = 5;
+	private $cHealth = 5;
+	private $strength = 5;
+	private $resistance = 2;
+	
+	private $x = 0;
+	private $y = 0;
+	
+	function __construct($x, $y) {
+		$this->x = $x;
+		$this->y = $y;
+		
+		$this->name = 'Tom';//RANDOMIZE
+	}
+	
+	function __destruct() {}
+	function destroy() { $this->__destruct(); }
+	
+	function getType() { return $this->type; }
+	function getHP() { return $this->cHealth; }
+	function getMHP() { return $this->health; }
+	function getAtt() { return $this->strength; }
+	function getDef() { return $this->resistance; }
+}
+
+class cat extends unit {
+	private $type = 'cat';
+	public $short = 'CAT';
+	private $health = 2;
+	private $cHealth = 2;
+	private $strength = 8;
+	private $resistance = 1;
+}
 ?>
 <!DOCTYPE html>
 <head>
@@ -32,7 +86,7 @@ $width = $dom->xpath('/board/@width')[0];
 			<?php
 			for ($h = 0; $h < $height; $h++) {
 				for ($w = 0; $w < $width; $w++) {
-					echo "<span class=\"tile\" id=\"".$h."_".$w."\"><span class=\"text\">".trim($dom->xpath('/board/tile')[($h * $width) + $w])."</span></span>";
+					echo "<span class=\"tile\" id=\"".$h."_".$w."\"><span class=\"text\">".($board->getElementsByTagName("tile")->item(($h*$width)+$w)->getElementsByTagName("unit")->length==0?"":$board->getElementsByTagName("tile")->item(($h*$width)+$w)->getElementsByTagName("unit")->item(0)->getAttribute("type"))."</span></span>";
 				}
 				echo "<br>";
 			}
